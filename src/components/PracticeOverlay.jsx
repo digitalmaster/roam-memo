1a1bab7473b1b50fb2903a6f290c2c50220bde19
Jose Browne <josebrowne@gmail.com>
2022-08-18 16:35:17 -0400

    Ui Tweaks

--- src/components/PracticeOverlay.jsx
+++ src/components/PracticeOverlay.jsx
@@ -6,6 +6,10 @@ const FooterWrapper = ({ children }) => (
   <div className="bp3-multistep-dialog-footer rounded-b-md">{children}</div>
 );
 
+const ContentWrapper = ({ children }) => (
+  <div className="bp3-dialog-body">{children}</div>
+);
+
 const PracticeOverlay = ({
   isOpen,
   onClose,
@@ -34,14 +38,17 @@ const PracticeOverlay = ({
       onClose={onClose}
       title="Review"
       className="pb-0"
+      icon="box"
     >
-      <div>{blockInfo.questionBlockString}</div>
+      <ContentWrapper>
+        <div className="mb-2">{blockInfo.questionBlockString}</div>
 
-      {showBlockChildren &&
-        hasBlockChildren &&
-        blockInfo.questionBlockChildren.map((childString, index) => (
-          <div key={index}>{childString}</div>
-        ))}
+        {showBlockChildren &&
+          hasBlockChildren &&
+          blockInfo.questionBlockChildren.map((childString, index) => (
+            <div key={index}>{childString}</div>
+          ))}
+      </ContentWrapper>
 
       <FooterWrapper>
         <div className="bp3-dialog-footer-actions justify-around">
@@ -49,6 +56,7 @@ const PracticeOverlay = ({
             <Blueprint.Button
               intent="none"
               onClick={() => setShowBlockChildren(true)}
+              outlined
             >
               Show Answer
             </Blueprint.Button>
@@ -57,18 +65,21 @@ const PracticeOverlay = ({
               <Blueprint.Button
                 intent="danger"
                 onClick={onGradeClick({ grade: 0 })}
+                outlined
               >
                 Forgot
               </Blueprint.Button>
               <Blueprint.Button
                 intent="warning"
                 onClick={onGradeClick({ grade: 3 })}
+                outlined
               >
                 Hard
               </Blueprint.Button>
               <Blueprint.Button
                 intent="success"
                 onClick={onGradeClick({ grade: 5 })}
+                outlined
               >
                 Perfect
               </Blueprint.Button>
