7eb1f65957334f174405a49df85565e9d1573c49
Jose Browne <josebrowne@gmail.com>
2022-08-20 11:30:43 -0400

    Use roam util helpers instead of DayJs

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,14 +1,10 @@
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import advancedFormat from 'dayjs/plugin/advancedFormat';
-dayjs.extend(customParseFormat);
-dayjs.extend(advancedFormat);
 export const getStringBetween = (string, from, to) =>
   string.substring(string.indexOf(from) + from.length, string.lastIndexOf(to));
 
 export const parseRoamDateString = (roamDateString) =>
-  dayjs(roamDateString, 'MMMM Do, YYYY').toDate();
+  window.roamAlphaAPI.util.pageTitleToDate(roamDateString);
 
-export const dateToRoamDateString = (jsDateObject) => dayjs(jsDateObject).format('MMMM Do, YYYY');
+export const dateToRoamDateString = (jsDateObject) =>
+  window.roamAlphaAPI.util.dateToPageTitle(jsDateObject);
 
 export const parseConfigString = (configString) => configString.split('::').map((s) => s.trim());
