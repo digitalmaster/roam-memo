a32e1d80c8ad2a15450b653019d0d03e9141da37
Jose Browne <josebrowne@gmail.com>
2022-08-22 15:56:26 -0400

    Add block props to create child block function

--- src/queries.js
+++ src/queries.js
@@ -130,6 +130,10 @@ export const fetchBlockInfo = async (refUid) => {
   };
 };
 
+/**
+ *  Shout out to David Bieber for these helpful functions
+ *  Blog: https://davidbieber.com/snippets/2021-02-12-javascript-functions-for-inserting-blocks-in-roam/
+ */
 const getPage = (page) => {
   // returns the uid of a specific page in your graph.
   // _page_: the title of the page.
@@ -225,7 +229,7 @@ const getChildrenBlocks = (parent_uid) => {
   }
 };
 
-const createChildBlock = async (parent_uid, block, order) => {
+const createChildBlock = async (parent_uid, block, order, blockProps = {}) => {
   // returns the uid of a specific child block underneath a specific parent block, creating it first if it's not already there.
   // _parent_uid_: the uid of the parent block.
   // _block_: the text of the child block.
@@ -235,7 +239,7 @@ const createChildBlock = async (parent_uid, block, order) => {
   }
   await window.roamAlphaAPI.createBlock({
     location: { 'parent-uid': parent_uid, order: order },
-    block: { string: block },
+    block: { string: block, ...blockProps },
   });
   let block_uid;
   while (!block_uid) {
@@ -245,33 +249,33 @@ const createChildBlock = async (parent_uid, block, order) => {
   return block_uid;
 };
 
-const createBlockOnPage = async (page, block, order) => {
+const createBlockOnPage = async (page, block, order, blockProps) => {
   // creates a new top-level block on a specific page, returning the new block's uid.
   // _page_: the title of the page.
   // _block_: the text of the block.
   // _order_: (optional) controls where to create the block, 0 for top of page, -1 for bottom of page.
   let page_uid = getPage(page);
-  return createChildBlock(page_uid, block, order);
+  return createChildBlock(page_uid, block, order, blockProps);
 };
 
-const getOrCreateBlockOnPage = async (page, block, order) => {
+const getOrCreateBlockOnPage = async (page, block, order, blockProps) => {
   // returns the uid of a specific block on a specific page, creating it first as a top-level block if it's not already there.
   // _page_: the title of the page.
   // _block_: the text of the block.
   // _order_: (optional) controls where to create the block, 0 for top of page, -1 for bottom of page.
   let block_uid = getBlockOnPage(page, block);
   if (block_uid) return block_uid;
-  return createBlockOnPage(page, block, order);
+  return createBlockOnPage(page, block, order, blockProps);
 };
 
-const getOrCreateChildBlock = async (parent_uid, block, order) => {
+const getOrCreateChildBlock = async (parent_uid, block, order, blockProps) => {
   // creates a new child block underneath a specific parent block, returning the new block's uid.
   // _parent_uid_: the uid of the parent block.
   // _block_: the text of the new block.
   // _order_: (optional) controls where to create the block, 0 for inserting at the top, -1 for inserting at the bottom.
   let block_uid = getChildBlock(parent_uid, block);
   if (block_uid) return block_uid;
-  return createChildBlock(parent_uid, block, order);
+  return createChildBlock(parent_uid, block, order, blockProps);
 };
 
 const getEmojiFromGrade = (grade) => {
@@ -291,10 +295,15 @@ const getEmojiFromGrade = (grade) => {
 
 export const savePracticeData = async ({ refUid, ...data }) => {
   await getOrCreatePage(getSettings().pluginPageTitle);
-  const dataBlockUid = await getOrCreateBlockOnPage(getSettings().pluginPageTitle, 'data', -1);
+  const dataBlockUid = await getOrCreateBlockOnPage(getSettings().pluginPageTitle, 'data', -1, {
+    open: false,
+    heading: 3,
+  });
 
   // Get child that matches refUid
-  const cardDataBlockUid = await getOrCreateChildBlock(dataBlockUid, `((${refUid}))`, 0);
+  const cardDataBlockUid = await getOrCreateChildBlock(dataBlockUid, `((${refUid}))`, 0, {
+    open: false,
+  });
 
   // Create new date child Note: Not using returned block id here because it
   // will be wrong if we practice the same question on the same day. This is
@@ -303,7 +312,9 @@ export const savePracticeData = async ({ refUid, ...data }) => {
   // top one
   const todayRoamDateString = stringUtils.dateToRoamDateString(new Date());
   const emoji = getEmojiFromGrade(data.grade);
-  await createChildBlock(cardDataBlockUid, `[[${todayRoamDateString}}]] ${emoji}`, 0);
+  await createChildBlock(cardDataBlockUid, `[[${todayRoamDateString}}]] ${emoji}`, 0, {
+    open: false,
+  });
   const updatedCardBlocks = getChildrenBlocks(cardDataBlockUid);
   const newDataBlockId = updatedCardBlocks.find((block) => block.order === 0).uid;
 
