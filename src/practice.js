94042629d3184ff7a5194c49dc8a1a50475522f8
Jose Browne <josebrowne@gmail.com>
2022-09-04 17:52:16 -0400

    Fork SM2 Algorithm
    So I can begin to make changes.

--- src/practice.js
+++ src/practice.js
@@ -1,6 +1,37 @@
-import { supermemo } from 'supermemo';
 import { savePracticeData } from '~/queries';
 import * as dateUtils from '~/utils/date';
+
+const supermemo = (item, grade) => {
+  let nextInterval;
+  let nextRepetition;
+  let nextEfactor;
+
+  if (grade >= 3) {
+    if (item.repetition === 0) {
+      nextInterval = 1;
+      nextRepetition = 1;
+    } else if (item.repetition === 1) {
+      nextInterval = 6;
+      nextRepetition = 2;
+    } else {
+      nextInterval = Math.round(item.interval * item.efactor);
+      nextRepetition = item.repetition + 1;
+    }
+  } else {
+    nextInterval = 1;
+    nextRepetition = 0;
+  }
+
+  nextEfactor = item.efactor + (0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02));
+
+  if (nextEfactor < 1.3) nextEfactor = 1.3;
+
+  return {
+    interval: nextInterval,
+    repetition: nextRepetition,
+    efactor: nextEfactor,
+  };
+};
 
 export const getPracticeResultData = ({ grade, interval, repetitions, eFactor }) => {
   const supermemoInput = {
