5e4163a65dd7a4b519f86285aca4d5eee1b45b34
Jose Browne <josebrowne@gmail.com>
2022-09-17 18:24:55 -0400

    Collapse memo data block references by default
    Was too noisy. Especially on days when you review lots of things.

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,8 @@ import practice from '~/practice.js';
 import usePracticeCardsData from '~/hooks/usePracticeCardsData.jsx';
 import useTags from '~/hooks/useTags.jsx';
 import useSettings from '~/hooks/useSettings';
+import * as asyncUtils from '~/utils/async';
+import * as domUtils from '~/utils/dom';
 
 const App = () => {
   const [showPracticeOverlay, setShowPracticeOverlay] = React.useState(false);
@@ -39,6 +41,28 @@ const App = () => {
   const handleMemoTagChange = (tag) => {
     setSelectedTag(tag);
   };
+
+  // Collapse memo data block ref by default (to keep things less noisy)
+  // @TODO: Maybe make this configurable
+  React.useEffect(() => {
+    const collapseDataReferenceBlock = async () => {
+      await asyncUtils.sleep(100);
+      const elm = [...document.querySelectorAll('.rm-ref-page-view .rm-ref-page-view-title')].find(
+        (elm) => elm.textContent === pluginPageTitle
+      );
+
+      const collapseControlBtn = elm?.parentNode.querySelector('.rm-caret-open');
+      collapseControlBtn && domUtils.simulateMouseClick(collapseControlBtn);
+    };
+    collapseDataReferenceBlock(); // trigger on page load
+    const onRouteChange = () => {
+      collapseDataReferenceBlock();
+    };
+    window.addEventListener('popstate', onRouteChange);
+    return () => {
+      window.removeEventListener('popstate', onRouteChange);
+    };
+  }, [pluginPageTitle]);
 
   return (
     <Blueprint.HotkeysProvider>
