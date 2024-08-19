2cf37ca884cdc4ac959aff8661bc5782bf1d8399
digitalmaster <josebrowne@gmail.com>
2024-08-19 17:42:00 -0400

    Ignore lottie type error

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,22 @@
 import '@testing-library/jest-dom';
 
+const originalConsoleError = console.error;
+
+// @ts-expect-error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Invalid prop/.test(args.toString())) {
+      return;
+    }
+    originalConsoleError(...args);
+  };
+});
+
+// @ts-expect-error
+afterAll(() => {
+  console.error = originalConsoleError;
+});
+
 module.exports = () => {
   process.env.TZ = 'UTC';
 };
