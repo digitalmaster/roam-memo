6dc6974ef525c9a678c0cadc39ef242c4fa9c2c5
Jose Browne <josebrowne@gmail.com>
2024-08-31 12:01:30 -0400

    fix timezone test issue

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom';
 
 const originalConsoleError = console.error;
 
-// @ts-expect-error
 beforeAll(() => {
   console.error = (...args) => {
     if (/Invalid prop/.test(args.toString())) {
@@ -12,11 +11,6 @@ beforeAll(() => {
   };
 });
 
-// @ts-expect-error
 afterAll(() => {
   console.error = originalConsoleError;
 });
-
-module.exports = () => {
-  process.env.TZ = 'UTC';
-};
