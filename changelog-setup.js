1936627d5f3152cbd0f03dca4df6769967d99d58
digitalmaster <josebrowne@gmail.com>
2025-03-15 16:32:43 -0400

    Bug Fix: Remove duplicates from changelog

--- changelog-setup.js
+++ changelog-setup.js
@@ -1,4 +1,7 @@
 module.exports = function (Handlebars) {
+  // Keep track of commits that have been included in specific sections
+  const processedCommits = new Set();
+
   Handlebars.registerHelper('full-commit-list', (commits, merges, fixes, options) => {
     commits = commits
       .concat(merges.map((merge) => merge.commit))
@@ -9,13 +12,20 @@ module.exports = function (Handlebars) {
     }
 
     const { exclude, message, subject, heading } = options.hash;
+    const isOtherChangesSection = exclude && exclude.includes('Feat:|Bug Fix:|Breaking change:');
 
     const list = commits
       .filter((item) => {
         const commit = item.commit || item;
+        const commitHash = commit.hash;
 
         // Exclude commits where subject includes with "Release v"
         if (commit.subject.includes('Release v')) {
+          return false;
+        }
+
+        // For "Other Changes" section, exclude commits that have been processed in specific sections
+        if (isOtherChangesSection && processedCommits.has(commitHash)) {
           return false;
         }
 
@@ -39,6 +49,8 @@ module.exports = function (Handlebars) {
           if (match) {
             // remove the message from the commit subject and message and trim
             commit.subject = commit.subject.replace(pattern, '').trim();
+            // Mark this commit as processed for a specific section
+            processedCommits.add(commitHash);
           }
           return match;
         }
